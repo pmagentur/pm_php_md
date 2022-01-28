c9bb2956d4d0071fe7f16aa0fda9f105fe9447a0
Hovhannes Vrtanesyan <hovhannes.vrtanesyan@pmagentur.com>
2022-01-28 13:30:25 +0400

    deleted annotation_max_number hendler

--- parse_phpmd.py
+++ parse_phpmd.py
@@ -2,7 +2,6 @@
 import json
 import sys
 import requests
-max_number_per_request = 50 # this is the max number of anotation per requset
 
 # Annotation should have this structure
 # Annotation = {                
@@ -19,8 +18,7 @@ max_number_per_request = 50 # this is the max number of anotation per requset
 
 
 def get_all_violations(json_file: str) -> dict:
-    violations = [] # for max_number_per_request annotations  
-    all_violations = [] # array of violations
+    violations = []  
     # Opening JSON file and load
     with open(json_file, 'r') as j:
      contents = json.loads(j.read())
@@ -31,21 +29,16 @@ def get_all_violations(json_file: str) -> dict:
         for violation in file["violations"]:
             violation_item = {"path": file["file"].replace(github_prefix,""), "start_line": violation["beginLine"],
          "end_line": violation["endLine"], "annotation_level": "warning", "message": violation["description"]}
-            if len(violations) < max_number_per_request:
-                violations.append(violation_item)
-            else:
-                all_violations.append(violations)
-                violations = []
-        all_violations.append(violations)
-    return all_violations
+            violations.append(violation_item)
+    return violations
 
 def update_pr(owner, repo_name, head_sha, file):
     URL = "https://pm-code-check.pm-projects.de/pm-checks/annotations/create"
     params = {"owner": owner, "repo_name": repo_name, "head_sha": head_sha, "check_name": "Phpmd check"}
     head = {"Content-Type'": "application/json"}
     all_violations = get_all_violations(file)
-    for violations in all_violations:
-        response = requests.post(URL, json=violations, params=params, headers=head)
+    response = requests.post(URL, json=all_violations, params=params, headers=head)
+    print(response)
 
 
 
